fdb9bb18ec81a995ea9b485f2c8aa378fc435f3f
okayu <yuto.gio.0515@gmail.com>
2024-12-01 16:42:12 +0900

    #1 モデル追加

---
+++ TodoBlazorApp/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoBlazorApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "タスク")]
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "30文字以内で入力してください。")]
+        [Required(ErrorMessage = "必須項目です。")]
+        public string Name { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+
+        public bool IsDelete { get; set; } = false;
+    }
+}
