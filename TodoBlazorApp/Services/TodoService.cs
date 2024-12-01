73156326c3c3f4cdb99ef8a7256df72d7a216512
okayu <yuto.gio.0515@gmail.com>
2024-12-02 00:33:00 +0900

    #2 一覧表示処理実装

---
+++ TodoBlazorApp/Services/TodoService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoBlazorApp.Data;
+using TodoBlazorApp.Models;
+
+namespace TodoBlazorApp.Services
+{
+    public class TodoService
+    {
+        private readonly TodoBlazorAppContext _context;
+
+        public TodoService(TodoBlazorAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Todo>> GetTodosAsync()
+        {
+            return await _context.Todo.Where(todo => todo.IsDone == false && todo.IsDelete == false).ToListAsync();
+        }
+
+        public async Task<List<Todo>> GetDoneTodosAsync()
+        {
+            return await _context.Todo.Where(todo => todo.IsDone == true && todo.IsDelete == false).ToListAsync();
+        }
+    }
+}
